e58a0185a541b0bebc9c5aba4e090df7b3174703
pvlach <vlach1989@gmail.com>
2020-02-19 12:45:55 +0100

    components & config update

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Test extends React.PureComponent {
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import Context from './context';
+import HoverHandler from './components/HoverHandler';
+import Popup from './components/HoverHandler/Popup';
+import Test from './components/Test';
+
+// TODO move somewhere else
+import {getTootlipPosition} from './components/HoverHandler/position';
 
 export {
-    Context
+    Context,
+    HoverHandler,
+    Popup,
+    Test,
+
+    getTootlipPosition
 }
