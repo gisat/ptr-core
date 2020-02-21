bfb060ed497b8af41b4dd06c9731357d3664d49f
pvlach <vlach1989@gmail.com>
2020-02-21 15:29:49 +0100

    context is singleton again - fix

--- src/context/index.js
+++ src/context/index.js
@@ -4,11 +4,15 @@ const Context = () => {
 
     let contexts = {};
 
-    let createContext = key => React.createContext();
-    let getContext = key => contexts[key] || createContext(key);
+    let createContext = (key, initialValue) => {
+        contexts[key] = React.createContext(initialValue);
+        return contexts[key];
+    };
+
+    let getContext = (key, initialValue) => contexts[key] || createContext(key, initialValue);
 
     return {
-
+        getContext
     }
 };
 
